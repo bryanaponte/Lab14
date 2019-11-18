0726499eaed9b55243ab77c5a9cb94d11b2785cb
Alumno <Alumno@LAB1504-18>
2019-11-18 15:37:37 -0500

    first commit

---
+++ Lab14/Lab14/TodoItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace Lab14
+{
+
+        public class TodoItem
+        {
+            [PrimaryKey, AutoIncrement]
+            public int ID { get; set; }
+            public int Dni { get; set; }
+            public string Nombre { get; set; }
+            public string Apellidos { get; set; }
+            public DateTime Fecha { get; set; }
+            public bool Done { get; set; }
+        }
+
+    
+}
